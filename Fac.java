b5900d87462d67c86ce6382368d24b241adffea8
pavithra1502 <pavithra.m1313081@ksrce.ac.in>
2016-07-20 20:46:44 +0530

    Create Fac.java

---
+++ Fac.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ public class Fac {
+ 	public static void main(String[] args) { 
+ 		  int i,f=1;  
+ 		  int n;   	  
+ 		Scanner s=new Scanner(System.in);
+ 		number=s.nextInt();
+ 		  for(i=1;i<=n;i++){ 
+ 			    f=f*i; 
+ 		}
+ 		  System.out.println("Factorial of "+n+" is: "+f);
+ 	}
