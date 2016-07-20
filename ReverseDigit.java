c099e95526a9ffc4e5c5434a53b84ab3010de72d
pavithra1502 <pavithra.m1313081@ksrce.ac.in>
2016-07-20 21:05:00 +0530

    Create ReverseDigit.java

---
+++ ReverseDigit.java
@@ -0,0 +1,12 @@
+public class ReverseDigit {
+	public static void main(String[] args) {
+		int n=12345;
+		int a = 0;
+		while(n!=0){
+			a=n%10;
+		System.out.print(a);
+			n=n/10;
+		}
+		}
+	
+	}
