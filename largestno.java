57a8f8ef6c86a6f8ef4cec82fb7587acb51aecef
pavithra1502 <pavithra.m1313081@ksrce.ac.in>
2016-07-20 16:12:58 +0530

    Create largestno.java

---
+++ largestno.java
@@ -0,0 +1,17 @@
+public class largestno {
+
+	public static void main(String[] args) {
+		int a=78,b=89,c=60;
+		if(a>b&&a>c){
+			System.out.println(a);
+		}
+		else if(b>c&&b>a){
+			System.out.println(b);
+		}
+		else{
+			System.out.println(c);
+		}
+
+	}
+
+}
