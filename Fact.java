1732610beca1d7ac07a9125a9d5b98b392a446de
pavithra1502 <pavithra.m1313081@ksrce.ac.in>
2016-07-20 20:44:32 +0530

    Update and rename Factorial.java to Fact.java

---
+++ Fact.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Fact {
+	public static void main(String[] args) { 
+		  int i,f=1;  
+		  int n;   	  
+		Scanner s=new Scanner(System.in);
+		number=s.nextInt();
+		  for(i=1;i<=n;i++){ 
+			    f=f*i; 
+		}
+		  System.out.println("Factorial of "+n+" is: "+f);
+	}
+
+}
+	
