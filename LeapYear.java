8cec72c369b56843f49bc97d4051560e0a24deb9
pavithra1502 <pavithra.m1313081@ksrce.ac.in>
2016-07-20 15:29:41 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,13 @@
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+	int a=1996;
+	if(a%4==0)
+		System.out.println("leap year" );
+	else
+		System.out.println("not a leap year");
+
+	}
+
+}
